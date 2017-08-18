a624fb870d704ab99f7388c89be7bb257f3057b9
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2017-08-18 20:30:24 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { AreaComponent } from './area/area.component';
+import { NvD3Module } from 'ng2-nvd3';
+import 'd3';
+import 'nvd3';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NvD3Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
