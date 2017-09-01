846f5c970c37f83aca7228e048521222a81a28a5
Sebastian Superczynski <ssuperczynski@dhapdigital.com>
2017-09-02 00:55:32 +0200

    generator

--- src/app/area/area.component.ts
+++ src/app/area/area.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {species, parties, places} from 'fantastical';
 
 @Component({
   selector: 'app-area',
@@ -8,12 +9,54 @@ import {Component, OnInit} from '@angular/core';
   ],
 })
 
-export class AreaComponent implements OnInit {
+export class AreaComponent {
+
+  // @Input() amount;
 
   constructor() {
+    this.fillArray(15);
   }
 
-  ngOnInit() {
+  graphData = {
+    nodes: [],
+    edges: []
+  };
+
+  generate(amount) {
+    this.clearArray();
+    this.fillArray(amount);
   }
 
+  clearArray() {
+    this.graphData = {
+      nodes: [],
+      edges: []
+    };
+  }
+
+  fillArray(amount) {
+    const names: string[] = [];
+    Array.apply(null, Array(amount)).map(() => {
+      const name = species.gnome();
+      console.log(name);
+      names.push(name);
+      this.graphData.nodes.push({
+        data: {
+          id: name,
+          name: name,
+          faveColor: '#D6DBDF',
+          faveShape: 'circle'
+        },
+      });
+    });
+    names.map((name, i) => {
+      this.graphData.edges.push({
+        data: {
+          source: name,
+          target: name,
+          faveColor: '#EAECEE'
+        }
+      });
+    });
+  }
 }
